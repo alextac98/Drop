978a92830f28d014bab989a7b02907d33bddf5ec
Alex Tacescu <alextac98@gmail.com>
2020-01-17 23:14:49 -0500

    Add comment to place URL in the console when node.js runs

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ function startServer() {
     app.use(express.static('public'));
 
     app.listen(3000, function () {
-        console.log("Example app listening on port 3000!")
+        console.log("Example app listening on port 3000!");
+        console.log("See it here: http://localhost:3000/");
     });
 };
 
